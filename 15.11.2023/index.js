8a3d1c170379c5ff174042d1dc2cec2deeb3f3a2
grzegorz szczepkowski <89992153+yrzer@users.noreply.github.com>
2023-11-19 23:21:39 +0100

    r

---
+++ 15.11.2023/index.js
@@ -0,0 +1,34 @@
+function mojeF_api(id, element) {
+    id = parseInt(id);
+    return fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+        .then(response => response.json())
+        .then(json => {
+            let w ="id = " + json.id + "<br>tytuł = "+ json.title+"<br> text"+json.body;
+            document.getElementById(element).innerHTML = w;
+            // zapis(json.userId,json.id,json.title,json.body);
+        });
+}
+let id = 0;
+// var on_post = {};
+function a(cos){
+    if(id+cos >0 && id+cos <=100){
+    mojeF_api(id+cos,"wynik")
+    id+=cos;}
+}
+
+// function zapis(userId,id,title,body){
+//     on_post = {
+//         "userId": userId,
+//         "id": id,
+//         "title": title,
+//         "body": body
+//     }
+//     // console.log(on_post.title)
+// }
+// function a(cos){
+//     if(id+cos >0 && id+cos <=100){
+//         id+=cos;
+//         mojeF_api(id,"wynik");
+//     }
+//     document.getElementById("wynik").innerHTML = "id = " + on_post.id + "<br>tytuł = "+ on_post.title+"<br> text"+on_post.body;
+// }
---
+++ zad- 19 listopada/index.js
@@ -0,0 +1,57 @@
+const books = [
+    {title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7},
+    {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4},
+    {title: 'Big loss', pages: 400, genre: 'fantasy', rating: 2},
+    {title: 'Tenth Joy', pages: 32, genre: 'action', rating: 9},
+    {title: 'Quickfix number 4', pages: 15, genre: 'fantasy', rating: 8},
+    {title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1},
+    {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5},
+];
+
+// Zad6. (2 pkt)
+// Stwórz kompozycję która na tablicy „books” zlicza ilość liter w tytule (title) nie licząc spacji,
+// gdzie strony (pages) są parzyste oraz genre kończy się na literkę „y”. 
+
+const zad6 = (tab)=> { // tablica
+    const r = tab.map((l) =>{ // line
+        if(l.pages%2 == 0 && l.genre[l.genre.length - 1]=="y"){
+            let sum=0;
+            for(x=0;x<l.title.length;x++)  if(l.title[x] != " ") sum++;  
+            return sum;
+        } else return null;
+    });
+    return r.reduce((a, x) => a + x);
+};
+console.log(zad6(books));
+// Zad7. (2 pkt)
+// Stwórz kompozycję która na tablicy „books” zlicza ilość pozytywnych ocen (rating > 5), gdzie
+// strony (pages) są liczbą nieparzystą oraz tytuł (title) zawiera liczbę.
+
+const zad7 = (tab)=> {
+    let sum=0;
+    tab.map((l) =>{ // line
+        if(l.pages%2 != 0 && l.rating > 5){
+            let t= false;
+            for(x=0;x<l.title.length;x++) {
+                if(parseInt( l.title[x] ) != NaN) t=true; 
+            }
+            if(t) sum++;
+        };
+    });
+    return sum;
+}
+console.log(zad7(books));
+// Zad8. (2 pkt)
+// Stwórz kompozycję która na tablicy „books” zwraca drugi najdłuższy tytuł (title).
+
+const zad8 = (tab)=> {
+    let n1="";
+    let n2="";
+    tab.map((l) =>{ // line
+        x = l.title; y = x.length
+        if(n1.length<y) n1 = x;
+        else if (n2.length<y) n2 = x;
+    });
+    return n2;
+}
+console.log(zad8(books));
